453cf5fd4abab51dcc68920813709ce5510a30f2
PC675 <sadriddinsalohutdinov@gmail.com>
2022-07-07 12:21:30 +0500

    full1

--- app/src/main/java/com/example/project78/MainActivity2.java
+++ app/src/main/java/com/example/project78/MainActivity2.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -11,5 +12,11 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Button login = findViewById(R.id.login_button);
+
+        login.setOnClickListener(v -> {
+
+        });
     }
 }
--- app/src/main/java/com/example/project78/MainActivity3.java
+++ app/src/main/java/com/example/project78/MainActivity3.java
@@ -3,6 +3,10 @@ package com.example.project78;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        EditText password = findViewById(R.id.password);
+        EditText re_password = findViewById(R.id.re_password);
+        TextView alert = findViewById(R.id.alert);
+        Button signup = findViewById(R.id.signup_button);
+
+        signup.setOnClickListener(v -> {
+            if (!(password.getText().toString().equals(re_password.getText().toString()))) {
+                alert.setVisibility(View.VISIBLE);
+            } else {
+                alert.setVisibility(View.GONE);
+            }
+        });
     }
+
 }
